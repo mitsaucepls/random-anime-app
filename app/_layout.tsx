1712eaf3c40f0c42ef3f5069a9ddf7a81430865d
Moritz Bosch <anmoritzbosch@gmail.com>
2025-01-09 13:17:41 +0100

    [feat] adds icons and removes title of pages in navbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,13 +4,24 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 export default function TabLayout() {
   return (
     <Tabs initialRoutName= "home">
-      <Tabs.Screen name="game" />
+      <Tabs.Screen
+        name="game"
+        options={{
+            headerShown: false,
+            tabBarLabel: () => null,
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="gamepad" color={color} />}}/>
       <Tabs.Screen
         name="home"
         options={{
             headerShown: false,
+            tabBarLabel: () => null,
             tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />}}/>
-      <Tabs.Screen name="chat" />
+      <Tabs.Screen
+        name="chat"
+        options={{
+            headerShown: false,
+            tabBarLabel: () => null,
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="comment" color={color} />}}/>
     </Tabs>
   );
 }
