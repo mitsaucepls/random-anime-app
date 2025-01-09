f08cc090a345ff1aaa290928a4977a55d8d22d2f
Moritz Bosch <anmoritzbosch@gmail.com>
2025-01-09 12:56:11 +0100

    [feat] feed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
-import { Stack } from "expo-router";
+import { Tabs } from 'expo-router';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabLayout() {
+  return (
+    <Tabs initialRoutName= "home">
+      <Tabs.Screen name="game" />
+      <Tabs.Screen
+        name="home"
+        options={{
+            headerShown: false,
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />}}/>
+      <Tabs.Screen name="chat" />
+    </Tabs>
+  );
 }
+
