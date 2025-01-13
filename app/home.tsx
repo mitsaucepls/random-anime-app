9962c59554e7a0d3028d3d4d17b4ef4a2ce5356d
BB223 <79644542+BB223@users.noreply.github.com>
2025-01-13 12:19:54 +0100

    feat: platform agnostic

--- app/home.tsx
+++ app/home.tsx
@@ -1,5 +1,5 @@
 import AnimeModel from "@/components/AnimeModel";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { Platform, SafeAreaView, StyleSheet, Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default () => {
@@ -7,28 +7,33 @@ export default () => {
         <SafeAreaProvider>
             <SafeAreaView style={styles.container}>
                 <Text style={styles.title}>AnimeModel</Text>
-                <AnimeModel />
+                {Platform.OS !== 'web' ? (
+                    <AnimeModel />
+                ) : (
+                    <Text>Model not supported</Text>
+                )
+                }
             </SafeAreaView>
         </SafeAreaProvider>
     );
 }
 
 const styles = StyleSheet.create({
-container: {
-    flex: 1,
-    padding: 24,
-    backgroundColor: '#eaeaea',
-  },
-  title: {
-    marginTop: 16,
-    paddingVertical: 8,
-    borderWidth: 4,
-    borderColor: '#20232a',
-    borderRadius: 6,
-    backgroundColor: '#61dafb',
-    color: '#20232a',
-    textAlign: 'center',
-    fontSize: 30,
-    fontWeight: 'bold',
-  },
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: '#eaeaea',
+    },
+    title: {
+        marginTop: 16,
+        paddingVertical: 8,
+        borderWidth: 4,
+        borderColor: '#20232a',
+        borderRadius: 6,
+        backgroundColor: '#61dafb',
+        color: '#20232a',
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
 })
