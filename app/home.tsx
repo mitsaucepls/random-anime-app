fe93b32fb961ec0cc90ecd41bef12ef373fbb644
BB223 <79644542+BB223@users.noreply.github.com>
2025-01-13 01:11:26 +0100

    waifu it is

--- app/home.tsx
+++ app/home.tsx
@@ -1,65 +1,47 @@
-import { Text, View } from "react-native";
-import ReactLive2d from '../CubismWebSamples/Samples/TypeScript/Demo/src/index.js';
-// import { useEffect, useState } from "react";
 import { WebView } from 'react-native-webview';
 import ReactDOMServer from 'react-dom/server';
 
 const AnimeModel: React.FC = () => {
-  return (
-   <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100%',
-      }}
-    >
-      <p>Anime Model</p>
-      <ReactLive2d
-        width={300}
-        height={500}
-        bottom={'10px'}
-        right={'10px'}
-        ModelList={['Haru', 'Hiyori']}
-        TouchBody={['啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊你要干嘛', '哼', '坏人']}
-      />
-    </div>
-  );
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%',
+            }}
+        >
+            <p>Anime Model</p>
+        </div>
+    );
 };
 
 
-export default function Index() {
-  const htmlContent = ReactDOMServer.renderToString(<AnimeModel />)
-  const htmlTemplate = `
-    <!DOCTYPE html>
-    <html>
-      <head>
+export default () => {
+    const htmlContent = ReactDOMServer.renderToString(<AnimeModel />)
+    const htmlTemplate = `
+<!DOCTYPE html>
+<html>
+    <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
-          body { font-family: Arial, sans-serif; margin: 0; padding: 0; }
+        body { font-family: Arial, sans-serif; margin: 12% 10px 0px 10px; padding: 0; }
         </style>
-      </head>
-      <body>
-        ${htmlContent}
-      </body>
-    </html>
+        <script src="https://fastly.jsdelivr.net/gh/samunatsu/live2d-widget-enhanced@latest/dist/core.min.js"></script>
+    </head>
+    <body>
+    ${htmlContent}
+        <script>
+        l2dwe.init({
+            api: new l2dwe.RemoteCdn('https://fastly.jsdelivr.net/gh/fghrsh/live2d_api'),
+            resource: 'https://fastly.jsdelivr.net/gh/samunatsu/live2d-widget-enhanced@latest/dist',
+        });
+        </script>
+    </body>
+</html>
   `;
-  return (
-    <WebView
-      originWhitelist={['*']}
-      source={{ html: htmlTemplate }}
-    />
-  );
+
+    return (
+        <WebView originWhitelist={['*']} source={{ html: htmlTemplate, baseUrl: 'http://localhost' }} webviewDebuggingEnabled />
+    );
 }
-//   const [release,setRelease] = useState(false)
-
-//   // function handleClick() {
-//   //     setRelease(true)
-//   //     props.history.push({ pathname: "/Other" });
-//   // }
-//   useEffect(() => {
-
-//   }, []);
-//   return (
-//   );
-// }
