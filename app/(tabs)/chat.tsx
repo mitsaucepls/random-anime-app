efa24361df13d88799850a725b7cfe8f5cf91ed1
Markus Siegert <spongebobmarkus@gmail.com>
2025-04-01 13:53:53 +0200

    ci: cool chat

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,10 +1,17 @@
-import { SafeAreaView } from 'react-native';
+import AnimeModel from '@/components/AnimeModel';
 import ChatLLM from '@/components/ChatLLM';
+import { Platform, SafeAreaView } from 'react-native';
+import { Text } from 'react-native-paper';
 import '@/global.css';
 
-export default function Chat() {
+export default function Index() {
   return (
-    <SafeAreaView className="flex-1 items-center">
+    <SafeAreaView className="flex-1">
+      {Platform.OS !== 'web' ? (
+        <AnimeModel />
+      ) : (
+        <Text className="text-9xl text-red-500">Model not supported</Text>
+      )}
       <ChatLLM />
     </SafeAreaView>
   );
--- components/AnimeModel.tsx
+++ components/AnimeModel.tsx
@@ -22,6 +22,75 @@ export default function AnimeModel({ style, ...otherProps }: WebViewProps) {
             #waifu-tips span {
                 display: none !important;
             }
+            #waifu {
+              bottom: -1000px;
+              left: 50%;
+              line-height: 0;
+              margin-bottom: -10px;
+              position: fixed;
+              transform: translate(-50%, 3px) scale(1.5);
+              transform-origin: bottom center;
+              transition: transform 0.3s ease-in-out, bottom 3s ease-in-out;
+              z-index: 1;
+            }
+            #waifu-tool {
+              bottom: 0;
+              color: #aaa;
+              opacity: 1;
+              position: absolute;
+              top: 70px;
+              transition: opacity 1s;
+            }
+            #waifu-toggle {
+              background-color: #fa0;
+              border-radius: 5px;
+              bottom: 66px;
+              color: #fff;
+              cursor: pointer;
+              font-size: 12px;
+              left: 0;
+              margin-left: -100px;
+              padding: 5px 2px 5px 5px;
+              position: fixed;
+              transition: margin-left 1s;
+              width: 60px;
+              writing-mode: vertical-rl;
+            }
+            #waifu-toggle.waifu-toggle-active {
+              margin-left: -50px;
+            }
+            #waifu-toggle.waifu-toggle-active:hover {
+              margin-left: -30px;
+            }
+            #waifu:hover {
+              transform: translate(-50%, 0) scale(1.5);
+            }
+            #live2d {
+              cursor: grab;
+              height: 300px;
+              position: relative;
+              width: 300px;
+            }
+            #live2d:active {
+              cursor: grabbing;
+            }
+            #waifu:hover #waifu-tool {
+              opacity: 1;
+            }
+            #waifu-tool span {
+              display: block;
+              height: 30px;
+              text-align: center;
+            }
+            #waifu-tool svg {
+              fill: #7b8c9d;
+              cursor: pointer;
+              height: 25px;
+              transition: fill 0.3s;
+            }
+            #waifu-tool svg:hover {
+              fill: #0684bd; /* #34495e */
+            }
         </style>
     </head>
     <body>
@@ -34,7 +103,10 @@ export default function AnimeModel({ style, ...otherProps }: WebViewProps) {
       originWhitelist={['*']}
       source={{ html: htmlTemplate, baseUrl: 'http://localhost' }}
       webviewDebuggingEnabled
-      style={[{ backgroundColor: 'transparent' }, { marginBottom: 10 }, style]}
+      style={[
+        { backgroundColor: 'transparent', marginBottom: 10 },
+        style,
+      ]}
       {...otherProps}
     />
   );
